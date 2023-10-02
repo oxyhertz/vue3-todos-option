ec33b3380e561cd537773d569cdbd4014fb77581
Dima Polo <prolonchuk5@gmail.com>
2023-10-02 22:37:17 +0300

    svg plugin

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import './style.css'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+
+import { clickOutside } from './directives'
+import { svgPlugin } from './plugins/svg.plugin'
 const app = createApp(App)
+app.directive('click-outside', clickOutside)
+app.use(svgPlugin)
 app.use(router)
 app.use(store)
 app.mount('#app')
